julianav13/Numpy

MAINT: Apply ruff/pycodestyle rule E743
E743 Ambiguous function name

numpy/__init__.pyi
@@ -4708,7 +4708,7 @@ class matrix(ndarray[_Shape2DType_co, _DType_co]):
     @property
     def T(self) -> matrix[_Shape2D, _DType_co]: ...
     @property
-    def I(self) -> matrix[_Shape2D, Any]: ...
+    def I(self) -> matrix[_Shape2D, Any]: ...  # noqa: E743
     @property
     def A(self) -> ndarray[_Shape2DType_co, _DType_co]: ...
     @property

Jack-D26/numpy_clone

numpy/__init__.pyi
@@ -5044,7 +5044,7 @@ class matrix(ndarray[_2DShapeT_co, _DType_co]):
     @property
     def T(self) -> matrix[_2D, _DType_co]: ...
     @property
-    def I(self) -> matrix[_2D, Any]: ...
+    def I(self) -> matrix[_2D, Any]: ...  # noqa: E743
     @property
     def A(self) -> ndarray[_2DShapeT_co, _DType_co]: ...
     @property